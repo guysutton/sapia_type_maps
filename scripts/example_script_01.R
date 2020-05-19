cb8a6adc25ad6723290a2f5980be5aaadf9462e3
guysutton <g.sutton@ru.ac.za>
2020-05-19 16:43:02 +0200

    Add Acacia cyclops example script

--- scripts/example_script_01.R
+++ scripts/example_script_01.R
@@ -1,16 +1,33 @@
 # Example functionality of map_sapia function, including downloading GBIF records 
+# - Example downloads GBIF records for Acacia cyclops, 
+#   and then makes three different maps (all records, presence/absence and abundance). 
 
-# Load a function from my GutHub
+# Load a function from my GitHub to download GBIF records from across South Africa 
 devtools::source_url("https://raw.githubusercontent.com/guysutton/sapia_type_maps/master/functions/get_clean_obs_function.R")
 
 # This code is not yet a function. I will do this soon.
 # It is just an example of how to use the sapa_type_map function
 # by downloading GBIF data, as we obviously don't have access to SAPIA data yet. 
-# Would be helluva cool though to have the SAPIA database pre-loaded into an 
-# R package with this functionality. 
 
 # You can try it for any species of interest below, just make sure you change species
 # name. 
+
+# Load packages
+library(tidyverse)
+library(sf)
+library(rnaturalearth)
+library(rnaturalearthdata)
+library(rlang)
+library(ggspatial)
+library(raster)
+
+# Import base map
+world <- rnaturalearth::ne_countries(scale = "medium",  
+                                     returnclass = "sf")
+
+# Keep only South Africa 
+world <- world %>%
+  dplyr::filter(name == "South Africa")
 
 ##############################################################
 # - Example #1: Plot distribution of Acacia cyclops in RSA
@@ -51,7 +68,8 @@ head(gps_filter)
 # ----- Run the map_sapia function 
 
 # Make sure the function is loaded into your workspace 
-
+# - Currently, you can load the function from my GitHub account 
+devtools::source_url("https://raw.githubusercontent.com/guysutton/sapia_type_maps/master/scripts/working_functions/01_sapia_type_map.R")
 
 # Map type #1: Plot all records
 map_sapia(data = gps_filter, 
@@ -71,6 +89,7 @@ map_sapia(data = gps_filter,
           map_type = "presence",
           # Which species should we plot? 
           species = "Acacia cyclops")
+
 ggsave("./figures/example_script_01_map_acacia_cyclops_pres.png",
        width = 8,
        height = 6,
@@ -82,6 +101,7 @@ map_sapia(data = gps_filter,
           map_type = "abundance",
           # Which species should we plot? 
           species = "Acacia cyclops")
+
 ggsave("./figures/example_script_01_map_acacia_cyclops_abun.png",
        width = 8,
        height = 6,
